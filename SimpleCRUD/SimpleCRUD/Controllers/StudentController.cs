f0c57a32b3cf7cda65b0ff48b57ae9d4f4a2a47c
Shohrat-Code <shohrat@code.edu.az>
2021-11-03 17:52:37 +0400

    Codes

---
+++ SimpleCRUD/SimpleCRUD/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCRUD.Data;
+using SimpleCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Students.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("index");
+            }
+            
+            return View(model);
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View(_context.Students.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Update(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("index");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _context.Students.Remove(_context.Students.Find(id));
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ SimpleCRUD/SimpleCRUD/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCRUD.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students{ get; set; }
+    }
+}
